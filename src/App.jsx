5f8ad06bb5df67457af1cf9d62b270f179f08dcd
Jatin Satare <jatinsatare@gmail.com>
2026-02-20 23:34:43 +0530

    add guessedLetter state to the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from "react"
 import { languages } from "./languages"
 
 export default function AssemblyEndgame() {
-    const [currentWord, setCurrentWord] = useState("react")
+  const [currentWord, setCurrentWord] = useState("react")
 
-      const letterElements = currentWord.split("").map((letter, index) => (
-        <span key={index}>{letter.toUpperCase()}</span>
-    ))
+  const [guessedLetters, setGuessedLetters] = useState([])
 
-    
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  function addGuessedLetter(letter) {
+    setGuessedLetters(prevLetters => [...prevLetters, letter])
+  }
+
+  const letterElements = currentWord.split("").map((letter, index) => (
+    <span key={index}>{letter.toUpperCase()}</span>
+  ))
+
+
 }
