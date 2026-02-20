2673bda5c1d7ee825015dd187be7d585898a44b3
Jatin Satare <jatinsatare@gmail.com>
2026-02-20 23:33:34 +0530

    add current word state to the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,11 @@ import { useState } from "react"
 import { languages } from "./languages"
 
 export default function AssemblyEndgame() {
-  
+    const [currentWord, setCurrentWord] = useState("react")
+
+      const letterElements = currentWord.split("").map((letter, index) => (
+        <span key={index}>{letter.toUpperCase()}</span>
+    ))
+
+    
 }
