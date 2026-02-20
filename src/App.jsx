cc5c5b6fbfa0ead1ee540f1c2c2b605acc23f027
Jatin Satare <jatinsatare@gmail.com>
2026-02-20 23:46:07 +0530

    update App.jsx for render functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,67 +2,70 @@ import { useState } from "react"
 import { languages } from "./languages"
 
 export default function AssemblyEndgame() {
-  const [currentWord, setCurrentWord] = useState("react")
+    const [currentWord, setCurrentWord] = useState("react")
+    const [guessedLetters, setGuessedLetters] = useState([])
+    console.log(guessedLetters)
 
-  const [guessedLetters, setGuessedLetters] = useState([])
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-  const alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-  function addGuessedLetter(letter) {
-    setGuessedLetters(prevLetters => [...prevLetters, letter])
-  }
-
-  const languageElements = languages.map(lang => {
-    const styles = {
-      backgroundColor: lang.backgroundColor,
-      color: lang.color
+    function addGuessedLetter(letter) {
+        setGuessedLetters(prevLetters => 
+            prevLetters.includes(letter) ? 
+                prevLetters : 
+                [...prevLetters, letter]
+        )
     }
+
+    const languageElements = languages.map(lang => {
+        const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color
+        }
+        return (
+            <span
+                className="chip"
+                style={styles}
+                key={lang.name}
+            >
+                {lang.name}
+            </span>
+        )
+    })
+
+    const letterElements = currentWord.split("").map((letter, index) => (
+        <span key={index}>{letter.toUpperCase()}</span>
+    ))
+
+    const keyboardElements = alphabet.split("").map(letter => (
+        <button
+            key={letter}
+            onClick={() => addGuessedLetter(letter)}
+        >
+            {letter.toUpperCase()}
+        </button>
+    ))
+
     return (
-      <span
-        className="chip"
-        style={styles}
-        key={lang.name}
-      >
-        {lang.name}
-      </span>
+        <main>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the
+                programming world safe from Assembly!</p>
+            </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
+            <section className="language-chips">
+                {languageElements}
+            </section>
+            <section className="word">
+                {letterElements}
+            </section>
+            <section className="keyboard">
+                {keyboardElements}
+            </section>
+            <button className="new-game">New Game</button>
+        </main>
     )
-  })
-
-  const letterElements = currentWord.split("").map((letter, index) => (
-    <span key={index}>{letter.toUpperCase()}</span>
-  ))
-
-  const keyboardElements = alphabet.split("").map(letter => (
-    <button
-      key={letter}
-      onClick={() => addGuessedLetter(letter)}
-    >
-      {letter.toUpperCase()}
-    </button>
-  ))
-
-  return (
-    <main>
-      <header>
-        <h1>Assembly: Endgame</h1>
-        <p>Guess the word within 8 attempts to keep the
-          programming world safe from Assembly!</p>
-      </header>
-      <section className="game-status">
-        <h2>You win!</h2>
-        <p>Well done! 🎉</p>
-      </section>
-      <section className="language-chips">
-        {languageElements}
-      </section>
-      <section className="word">
-        {letterElements}
-      </section>
-      <section className="keyboard">
-        {keyboardElements}
-      </section>
-      <button className="new-game">New Game</button>
-    </main>
-  )
-
 }
