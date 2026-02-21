734d633142bf5b5f62855150cb90cfd7ab764422
Jatin Satare <jatinsatare@gmail.com>
2026-02-21 22:32:08 +0530

    add keyboard color change functionality on guesses

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import { useState } from "react"
 import { languages } from "./languages"
+import clsx from "clsx";
+
+/**
+ * Goal: Allow the user to start guessing the letters
+ * 
+ * Challenge: Update the keyboard when a letter is right
+ * or wrong.
+ * 
+ * Bonus: use the `clsx` package to easily add conditional 
+ * classNames to the keys of the keyboard. Check the docs 
+ * to learn how to use it 📖
+ */
 
 export default function AssemblyEndgame() {
     const [currentWord, setCurrentWord] = useState("react")
@@ -8,11 +20,13 @@ export default function AssemblyEndgame() {
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
     function addGuessedLetter(letter) {
-        setGuessedLetters(prevLetters => 
-            prevLetters.includes(letter) ? 
-                prevLetters : 
+        setGuessedLetters(prevLetters =>
+            prevLetters.includes(letter) ?
+                prevLetters :
                 [...prevLetters, letter]
         )
+
+
     }
 
     const languageElements = languages.map(lang => {
@@ -35,21 +49,35 @@ export default function AssemblyEndgame() {
         <span key={index}>{letter.toUpperCase()}</span>
     ))
 
-    const keyboardElements = alphabet.split("").map(letter => (
-        <button
-            key={letter}
-            onClick={() => addGuessedLetter(letter)}
-        >
-            {letter.toUpperCase()}
-        </button>
-    ))
+    const keyboardElements = alphabet.split("").map(letter => {
+
+        const isGuessed = guessedLetters.includes(letter)
+        const isCorrect = isGuessed && currentWord.includes(letter)
+        const isWrong = isGuessed && !currentWord.includes(letter)
+
+        const className = clsx({
+            correct: isCorrect,
+            wrong: isWrong
+        })
+
+        return (
+            <button
+                key={letter}
+                onClick={() => addGuessedLetter(letter)}
+                className={className}
+            >
+                {letter.toUpperCase()}
+            </button>
+        )
+
+    })
 
     return (
         <main>
             <header>
                 <h1>Assembly: Endgame</h1>
                 <p>Guess the word within 8 attempts to keep the
-                programming world safe from Assembly!</p>
+                    programming world safe from Assembly!</p>
             </header>
             <section className="game-status">
                 <h2>You win!</h2>
